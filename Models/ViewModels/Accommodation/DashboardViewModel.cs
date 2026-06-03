356d8fc55b1f240ffda4b3ab30a14724b4e6ea60
nqv1208 <qvinhkl10@gmail.com>
2026-06-04 00:10:31 +0700

    feat(accommodation): implement accommodation owner dashboard, facilities updates, and staying declarations

---
+++ Models/ViewModels/Accommodation/DashboardViewModel.cs
@@ -0,0 +1,13 @@
+using ManagementOfForeigners.Models.Entities;
+
+namespace ManagementOfForeigners.Models.ViewModels.Accommodation;
+
+public class DashboardViewModel
+{
+    public CoSoLuuTru CoSoInfo { get; set; } = null!;
+    public int TotalStaying { get; set; }
+    public int TotalStayed { get; set; }
+    public int TotalOverdue { get; set; }
+    public List<LichSuLuuTru> ActiveStays { get; set; } = new();
+    public List<LichSuLuuTru> RecentCheckins { get; set; } = new();
+}
